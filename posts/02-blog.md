24a3d9347b60fc249bb892833b5b223f16c3c1ef
Erin Guetzloe <eguetzloe@college.harvard.edu>
2020-09-23 11:01:09 -0400

    thought this was already committed, but trying again

---
+++ posts/02-blog.md
@@ -0,0 +1,51 @@
+# Voting With Your Wallet: When and How Do Americans Engage in Economic Voting?
+
+Going into an election cycle, one of the most important questions posed by pundits is **how the state of the economy will impact the election**. It is generally believed that various aspects of the economy such as GDP growth, average real disposable income, and unemployment rates all have a role to play in election outcomes.
+
+But if we believe that voters do tend to “vote with their wallets,” this raises a number of questions. First of all, **do these measures of an economy have the same impact when a presidential candidate is an incumbent as when the candidate is a non-incumbent party successor?** If there seems to be a stronger correlation between economic predictors and an incumbent party’s popular vote share when the candidate is an incumbent rather than a successor, this could indicate that voters attribute economic success to individual candidates rather than parties as a whole.
+
+Another interesting question is **whether variables have changed in their predictive power over time.** Has the economy become a better or worse predictor of election results over the past 50 years, or has it remained relatively the same? 
+
+To investigate these questions, there are a wide variety of possible economic predictors that we could include. However, for today I have chosen to only look at unemployment’s impact on elections. The unemployment rate is one of the most heavily discussed topics on the campaign trail. Former Vice President Biden has [blamed President Trump](https://www.usatoday.com/story/news/politics/2020/06/05/joe-biden-slams-trump-spiking-ball-despite-high-unemployment/3153356001/) for not responding strongly enough to rising unemployment during the pandemic. At the same time, President Trump has [touted unemployment drops](https://www.politico.com/news/2020/06/05/may-unemployment-rate-coronavirus-302945) as sign of his administration’s ability to rally the economy. Considering how important unemployment seems to be to both candidates, I will only be exploring unemployment’s power as a predictor today.
+
+## Unemployment as a Predictor of Incumbent Party Vote Share
+
+![](../figures/overallincumbencyfx.png)
+
+The graphic shown above illustrates the correlation between national unemployment rates and the percentage of popular vote won by an incumbent party in presidential elections going back to 1976. A simple linear model explaining incumbent party vote share by national unemployment rates shows that there is a **slightly negative relationships between unemployment and incumbent vote share**. The model and graphic indicate that with every 1-percentage point increase in the amount of national unemployment, incumbent party vote share in a presidential election will decrease by approximately 0.24 percentage points. 
+
+While this relationship would generally be expected- higher levels of unemployment probably will not make voters more excited about voting for an incumbent party- **the strength of this relationship is called into question by very low R-squared values**. According to these R-squared values, only about 0.6% of the variation in incumbent vote share can be accounted for by this model. This calls into question the strength of unemployment alone as a predictor of electoral results.
+
+## Unemployment's Impact on Incumbent Candidates vs. Party Successors
+
+Another question I was curious to investigate was **whether unemployment rates had the same impacts on incumbent candidates and non-incumbent party successors.** To visualize this difference, I created another graphic, this time distinguishing between races with incumbent candidates seeking re-election and non-incumbent party successors.
+
+![](../figures/successorvsincumbent.png)
+
+According to the graphic and some summary statistics calculated for each linear model, the **impact of unemployment is roughly the same for both incumbent candidates and non-incumbent successors**. The effect on incumbents is just slightly stronger. With every 1-percentage point increase in unemployment, there is a correlated decrease of about 0.90 percentage points for incumbent candidates, but only about a 0.81 percentage point decrease for non-incumbents. However, this is a relatively minimal distinction, and yet again the **R-squared values found for both models are very small**. This tells us that neither model is particularly good at explaining the variation in incumbent party's vote share.
+
+## Unemployment's Predictive Power Over Time
+
+Though unemployment does not appear to be the best predictor of election outcomes as evidenced by the analysis above, I was interested in exploring whether unemployment has been a better predictor of certain years than others. The animation below shows individual states' various unemployment rates along with the percentage of the state's vote won by the incumbent party.
+
+![](../figures/unemploymentovertime.gif)
+
+Interestingly, in 6 out of 11 election years (a slim majority but a majority nonetheless!) the relationship between a state's vote share for the incumbent party and that state's unemployment rate was positive. Thus, **in 6 election years since 1976, higher unemployment rates in individual states were actually correlated with higher vote shares within those states for the incumbent party.** It is important to note, however, that for the election years where this positive relationship was present, the relationship was sometimes very weak. 
+
+To explore this further, I selected election years from each decade that data was available and created models using national unemployment rates to explain incumbent vote share. The results of these regressions are displayed in the table below.
+
+![](../figures/unemploymenthuxtable.png)
+
+Out of these election years, we see some interesting variations in unemployment's relationship with election outcomes. In 1976 and 2008, there was a negative relationship between incumbent vote share and national unemployment rates, which is the relationship most people would generally expect to see between these variables. However, the table above shows that yet again, the R-squared values for these years are low. 
+
+1988 shows a weakly positive relationship between incumbent vote share and unemployment, but the R-squared value calculated reveals that only 1.6% of the variation in incumbent vote share is explained by this model. 
+
+1996 seems to be a bit of an anomaly because of the relatively strong positive relationship between incumbent vote share and unemployment. In addition to the strength of this relationship, the R-squared value- while still low- is comparatively much higher than previous years analyzed, with 17.7% of the variation in vote share explained.
+
+Considering the variability in unemployment's relationship with incumbent vote share, it **does not seem that unemployment is the greatest predictor of election results.** However, I was still interested to see what a prediction based on unemployment and incumbent vote share since 1976 could reveal about the potential outcome of the 2020 election. The results of that prediction are shown in the table below.
+
+![](../figures/unemploymentpred.png)
+
+**According to this (very simplistic!) prediction of 2020 election outcomes based solely on unemployment, President Donald Trump would earn 50.26% of the popular vote.** This could be enough to scrape out a narrow victory. However, the 2.5% and 95% confidence intervals for this prediction estimate that **Trump's vote share in the election could be anywhere from about 39% to 62%. That margin is very wide and does not speak well to the predictive power of this model.**
+
+Though it appears that unemployment on its own may not be the best predictor of election outcomes, it is still a significant part of rhetoric on the campaign trails. Perhaps in combination with other economic factors such as GDP growth and real disposable income, models can use unemployment as one variable among others to more accurately predict election results.
--- scripts/02-Economy.R
+++ scripts/02-Economy.R
@@ -7,6 +7,8 @@ library(gganimate)
 library(transformr)
 library(gifski)
 library(janitor)
+library(huxtable)
+library(gt)
 # read in the csvs and assigned variable names
 popvote <- read_csv("data/popvote_1948-2016.csv")
 popvotestate <- read_csv("data/popvote_bystate_1948-2016.csv")
@@ -16,7 +18,7 @@ economy <- read_csv("data/econ.csv")%>%
   # Grouped by year and summarized to find mean national unemployment rates per
   # year
   group_by(year) %>%
-  summarize(nationalunemployment = mean(unemployment)) %>%
+  summarize(nationalunemployment = mean(unemployment))
 local <- read.csv("data/local.csv") %>%
   # Cleaned names to make work with variables easier
   clean_names() %>%
@@ -24,37 +26,25 @@ local <- read.csv("data/local.csv") %>%
   filter(!state_and_area %in% c("Los Angeles County", "New York city")) %>%
   # Renamed for easier work
   rename(state = state_and_area)
-# Joined three datasets
-localdata <- popvotestate %>% 
-  left_join(popvote, by = "year") %>% 
-  # Filtered because I am only interested in exploring incumbency impacts
-  filter(incumbent == TRUE) %>% 
-  left_join(local, by = c("state", "year")) %>%
-  #mutate(pv2p = case_when(
-    #party == "republican" ~ R_pv2p,
-    #party == "democrat" ~ D_pv2p
- # ))
 
-# Piped in local dataset  
-incumbencydata <- local %>%
-  # Grouped by state and year and then mutated to create an unemployment by
-  # state variable
-  group_by(state, year) %>%
-  mutate(unemploymentstatepct = mean(unemployed_prce)) %>%
-  # Joined economy dataset
-  left_join(economy, by = "year") %>%
-  # Joined popvote dataset
-  left_join(popvote, by = "year") %>%
-  # Selected relevant datasets
-  select(state, year, unemploymentstatepct, nationalunemployment, pv2p, incumbent, incumbent_party) %>%
-  # Filtered for only incumbent party vote share
+# Piped in economy data  
+incumbentdata <- economy %>%
+  # Joined popvote and economy datasets
+  full_join(popvote, by = "year") %>%
+  # Filtered for only election years
+  filter(year %in% c(1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020)) %>%
+  # Filtered for only incumbent vote share
   filter(incumbent_party == TRUE) %>%
-  filter(year == 1976)
+  # Selected needed variables
+  select(year, nationalunemployment, pv2p, incumbent, incumbent_party, party) 
 
 # Made basic lm model
-lm(data = incumbencydata, pv2p ~ nationalunemployment)
+basicmodel <- lm(data = incumbentdata, pv2p ~ nationalunemployment)
+# Created model summary
+summary(basicmodel)
 # Created ggplot of linear model
-ggplot(incumbencydata, aes(x=nationalunemployment, y=pv2p,
+overallincumbencyfx <-
+  ggplot(incumbentdata, aes(x=nationalunemployment, y=pv2p,
            label=year)) + 
   # Plotted points via text
   geom_text() +
@@ -70,7 +60,16 @@ ggplot(incumbencydata, aes(x=nationalunemployment, y=pv2p,
   # Changed font sizes
   theme(plot.title = element_text(size = 13), 
         plot.subtitle = element_text(size = 9))
+# Saved png
+ggsave("figures/overallincumbencyfx.png", height = 6, width = 6)
 
+
+lmdata <- incumbentdata %>%
+  filter(incumbent == TRUE)
+  incumbentlm <- lm(data = lmdata, pv2p ~ nationalunemployment)
+lm2data <- incumbentdata %>%
+    filter(incumbent == FALSE)
+  nonincumbentlm <- lm(data = lm2data, pv2p ~ nationalunemployment)
 # Took these steps to later change facet titles. I found this hack from this
 # data blog:
 # https://www.datanovia.com/en/blog/how-to-change-ggplot-facet-labels/#:~:text=(dose%20~%20supp)-,Change%20the%20text%20of%20facet%20labels,labeller%20function%20label_both%20is%20used.
@@ -78,7 +77,7 @@ truefalse <- c("Incumbent", "Non-Incumbent Successor")
 names(truefalse) <- c("TRUE", "FALSE")
 
 # Created ggplot
-incumbencydata %>%
+successorvsincumbent <- incumbentdata %>%
   ggplot(aes(x=nationalunemployment, y=pv2p,
                              label=year)) + 
   # Faceted by incumbent or party successor
@@ -90,9 +89,131 @@ incumbencydata %>%
   # Set labels
   labs(x = "National unemployment rate", 
        y = "Incumbent party popular vote share", 
-       title = "Effect of the Economy Based on Incumbency",
+       title = "Effect of Unemployment Based on Incumbency",
        subtitle = "Both vote share and unemployment rate values are measured in percentages.") +
   # Changed theme
   theme_bw() +
   # Changed font sizes for subtitle
   theme(plot.subtitle = element_text(size = 9))
+# Saved png
+ggsave("figures/successorvsincumbent.png", height = 6, width = 6)
+
+
+# Created state abbreviations tibble
+states <- tibble(state = state.name, abb = state.abb)
+
+# Piped in incumbent dataset used earlier
+statedata <- incumbentdata %>%
+  # Joined local economy dataset with state vote dataset
+  right_join(popvotestate, by = "year") %>%
+  # Created a variable 
+  mutate(incumbentstatevote = case_when(
+    party == "republican" ~ R_pv2p,
+    party == "democrat" ~ D_pv2p)) %>%
+  # Selected desired variables
+  select(year, nationalunemployment, pv2p, incumbent, incumbent_party, state, R_pv2p, D_pv2p, incumbentstatevote) %>%
+  # Joined with local economy dataset
+  left_join(local, by = c("state", "year")) %>%
+  # Grouped by state and year and mutated to create an unemployment by state
+  # variable
+  group_by(state, year) %>%
+  mutate(unemploymentstatepct = mean(unemployed_prce)) %>%
+  filter(year > 1975) %>%
+  left_join(states, by = "state") %>%
+  # Selected desired variables
+  select(year, nationalunemployment, pv2p, incumbent, incumbent_party, state, R_pv2p, D_pv2p, unemploymentstatepct, incumbentstatevote, abb) %>%
+  # Removed duplicate rows
+  distinct()
+
+unemploymentovertime <- statedata %>%
+# Created ggplot
+ggplot(aes(x = unemploymentstatepct, y = incumbentstatevote)) +
+  # Decreased scatterplot point size for cleaner look
+  geom_point(size = 0.5) +
+  # Created regression line and reduced line thickness
+  geom_smooth(method = "lm", size = 0.5) +
+  # Set titles
+  labs(x = "State unemployment rate", 
+       y = "State vote share won by incumbent party", 
+       title = "State Incumbent Vote Share Explained by Unemployment", 
+       subtitle = "Year: {current_frame}",
+       caption = "Both vote share and unemployment rate values are measured in percentages.") +
+  # Set theme
+  theme_bw() +
+  # Set font sizes
+  theme(plot.title = element_text(size = 7), plot.subtitle = element_text(size = 6), axis.title = element_text(size = 6), axis.text = element_text(size = 6), plot.caption = element_text(size = 6)) +
+  # Set transition style
+  transition_manual(year)
+# Animated graphic and manually set height, width, and resolution
+animate(unemploymentovertime, duration = 15, fps = 20, width = 3, height = 2, units="in", res= 175, renderer = gifski_renderer())
+# Saved gif
+anim_save("figures/unemploymentovertime.gif")
+
+
+# Made multiple models for various election years since 1976
+year1data <- statedata %>%
+  filter(year == 1976)
+model1 <- lm(data = year1data, incumbentstatevote ~ unemploymentstatepct)
+
+year2data <- statedata %>%
+  filter(year == 1988)
+model2 <- lm(data = year2data, incumbentstatevote ~ unemploymentstatepct)
+
+year3data <- statedata %>%
+  filter(year == 1996)
+model3 <- lm(data = year3data, incumbentstatevote ~ unemploymentstatepct)
+
+year4data <- statedata %>%
+  filter(year == 2008)
+model4 <- lm(data = year4data, incumbentstatevote ~ unemploymentstatepct)
+
+# Placed all models in a huxtable object
+huxtable <- huxreg("1976" = model1, 
+                   "1988" = model2, 
+                   "1996" = model3, 
+                   "2008" = model4,
+                   # Set new row titles
+                   coefs = c('Intercept'='(Intercept)',
+                             'Slope'='unemploymentstatepct'),
+                   statistics = c("R-Squared Values" = "r.squared"),
+                   # Set confidence intervals
+                   ci_level = .95,
+                   # Set confidence interval format
+                   error_format = '({conf.low} to {conf.high})') %>%
+  # Set title
+  set_caption('Incumbent Vote Share Explained by Unemployment')
+# Print table
+huxtable
+
+# Filtering for unemployment data from 2020 only
+unemployment2020 <- economy %>%
+  filter(year == 2020)
+# Created a tibble with unemployment data from 2020, predictions, and confidence
+# interval measurements
+tibble <- tibble(predictive_variable = "Unemployment",
+       unemploymentdata = unemployment2020$nationalunemployment,
+       pv2p = predict(basicmodel, unemployment2020),
+       lower = predict(basicmodel, unemployment2020, interval = "prediction")[2],
+       upper = predict(basicmodel, unemployment2020, interval = "prediction")[3])
+# Created a table of predictions
+gt(tibble) %>%
+  # Removed variable column
+  cols_hide(columns = "predictive_variable") %>%
+  # Labeled columns
+  cols_label(unemploymentdata = "Unemployment Rate", 
+             pv2p = "Incumbent Vote Share",
+             lower = "2.5 % CI",
+             upper = "95% CI") %>%
+  # Set decimal point limit
+  fmt_number(columns = 2:5, decimals = 2) %>%
+  # Set titles and subtitle
+  tab_header(title = "Prediction of 2020 Republican Vote Share Based on Unemployment",
+             subtitle = "Unemployment rate is the most recent as of September 20, 2020") %>%
+  # Set source note
+  tab_source_note(source_note = "Source: Bureau of Labor Statistics news release, published September 4, 2020") %>%
+  # Set footnote
+  tab_footnote(
+    footnote = "Unemployment rate and incumbent vote share are measured in percentages",
+    locations = cells_body(
+      columns = vars(unemploymentdata, pv2p),
+      rows = 1))
